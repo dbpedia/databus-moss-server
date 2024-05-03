0d197ead444ed4f9044a59541950afd6011b117e
holycrab13 <jan.forberg@hotmail.de>
2024-05-03 13:41:42 +0200

    first fixes

--- src/main/java/org/dbpedia/moss/Main.java
+++ src/main/java/org/dbpedia/moss/Main.java
@@ -1,30 +1,24 @@
 package org.dbpedia.moss;
 
 import org.dbpedia.moss.servlets.MetadataReadServlet;
+import org.dbpedia.moss.servlets.MetadataWriteServlet;
+import org.dbpedia.moss.servlets.MetadataAnnotateServlet;
+import org.dbpedia.moss.servlets.LogoutServlet;
 import org.eclipse.jetty.security.ConstraintMapping;
 import org.eclipse.jetty.security.ConstraintSecurityHandler;
 import org.eclipse.jetty.security.DefaultIdentityService;
 import org.eclipse.jetty.security.IdentityService;
-import org.eclipse.jetty.security.authentication.SessionAuthentication;
 import org.eclipse.jetty.security.openid.OpenIdAuthenticator;
 import org.eclipse.jetty.security.openid.OpenIdConfiguration;
 import org.eclipse.jetty.security.openid.OpenIdLoginService;
 import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.UserIdentity;
 import org.eclipse.jetty.server.handler.HandlerList;
 import org.eclipse.jetty.server.session.SessionHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.security.Constraint;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-
-import java.io.IOException;
 import java.util.Collections;
 
 /**
@@ -81,29 +75,25 @@ public class Main {
         security.setLoginService(loginService);
         security.setAuthenticator(openidAuthenticator);
 
-        // Context handler for the unprotected route
-        
-        ServletContextHandler context = new ServletContextHandler();
-        context.setContextPath("/*");
-        context.addServlet(new ServletHolder(new MetadataReadServlet()), "/g/*");
-        context.addServlet(new ServletHolder(new UnprotectedServlet()), "/unprotected2");
+        // Context handler for the unprotected routes
+        ServletContextHandler openContext = new ServletContextHandler();
+        openContext.setContextPath("/*");
+        openContext.addServlet(new ServletHolder(new MetadataReadServlet()), "/g/*");
+        openContext.addServlet(new ServletHolder(new MetadataAnnotateServlet()), "/api/annotate");
 
-        // Context handler for the protected route
-        //logoutContext.setSessionHandler(sessionHandler);
-        //logoutContext.setSecurityHandler(security);
+        // Context handler for the protected routes
          
         // Context handler for the protected route
         ServletContextHandler protectedContext = new ServletContextHandler();
-        protectedContext.setContextPath("/protected");
-        protectedContext.addServlet(new ServletHolder(new LogoutServlet(loginService)), "/logout");
-        protectedContext.addServlet(new ServletHolder(new ProtectedServlet()), "/*");
+        protectedContext.setContextPath("/*");
+        protectedContext.addServlet(new ServletHolder(new LogoutServlet(loginService)), "auth/logout");
+        protectedContext.addServlet(new ServletHolder(new MetadataWriteServlet()), "/api/save");
         protectedContext.setSessionHandler(sessionHandler);
         protectedContext.setSecurityHandler(security);
 
         // Set up handler collection
         HandlerList  handlers = new HandlerList();
-        handlers.setHandlers(new Handler[] { protectedContext, context });
-        // handlers.addHandler(protectedContext);
+        handlers.setHandlers(new Handler[] { protectedContext, openContext });
 
         server.setHandler(handlers);
 
@@ -111,68 +101,4 @@ public class Main {
         server.join();
     }
 
-    // Unprotected Servlet
-    private static class UnprotectedServlet extends HttpServlet {
-        @Override
-        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-            resp.setContentType("text/html");
-            resp.getWriter().println("<h1>This is an unprotected route</h1>");
-        }
-    }
-
-    // Protected Servlet
-    private static class ProtectedServlet extends HttpServlet {
-      
-
-        @Override
-        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-            resp.setContentType("text/html");
-            resp.getWriter().println("<h1>This is a protected route</h1>");
-    
-            // Add logout button
-            resp.getWriter().println("<form action='/protected/logout' method='post'>");
-            resp.getWriter().println("<input type='submit' value='Logout'>");
-            resp.getWriter().println("</form>");
-        }
-
-       
-    }
-
-    
-    // Protected Servlet
-    private static class LogoutServlet extends HttpServlet {
-      
-        private final OpenIdLoginService loginService;
-        
-        public LogoutServlet(OpenIdLoginService loginService) {
-            this.loginService = loginService;
-        }
-
-        @Override
-        protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-            resp.setContentType("text/html");
-            resp.getWriter().println("<h1>This is another protected route</h1>");
-            
-            HttpSession session = req.getSession(false); // false means do not create a new session if not exists
-
-            if (session != null) {
-                // Retrieve the SessionAuthentication from the HttpSession
-                SessionAuthentication authentication = (SessionAuthentication) session.getAttribute(SessionAuthentication.__J_AUTHENTICATED);
-                
-                if (authentication != null) {
-                    // Retrieve the UserIdentity from the SessionAuthentication
-                    UserIdentity userIdentity = authentication.getUserIdentity();
-                    
-                    // Now you have the UserIdentity, you can use it as needed
-                    loginService.logout(userIdentity);
-                }
-
-                session.invalidate();
-            }
-            
-
-            // Redirect the user to a logout confirmation page or any other appropriate page
-            resp.sendRedirect("/unprotected");
-        }
-    }
 }
---
+++ src/main/java/org/dbpedia/moss/servlets/LogoutServlet.java
@@ -0,0 +1,55 @@
+package org.dbpedia.moss.servlets;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import org.eclipse.jetty.security.authentication.SessionAuthentication;
+import org.eclipse.jetty.security.openid.OpenIdLoginService;
+import org.eclipse.jetty.server.UserIdentity;
+
+/**
+ * HTTP Servlet with handlers for the single lookup API request "/api/search"
+ * Post-processes requests and sends the query to the LuceneLookupSearcher,
+ * which translates requests into lucene queries
+ */
+
+// Protected Servlet
+public class LogoutServlet extends HttpServlet {
+    
+    private final OpenIdLoginService loginService;
+    
+    public LogoutServlet(OpenIdLoginService loginService) {
+        this.loginService = loginService;
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.getWriter().println("<h1>This is another protected route</h1>");
+        
+        HttpSession session = req.getSession(false); // false means do not create a new session if not exists
+
+        if (session != null) {
+            // Retrieve the SessionAuthentication from the HttpSession
+            SessionAuthentication authentication = (SessionAuthentication) session.getAttribute(SessionAuthentication.__J_AUTHENTICATED);
+            
+            if (authentication != null) {
+                // Retrieve the UserIdentity from the SessionAuthentication
+                UserIdentity userIdentity = authentication.getUserIdentity();
+                
+                // Now you have the UserIdentity, you can use it as needed
+                loginService.logout(userIdentity);
+            }
+
+            session.invalidate();
+        }
+        
+
+        // Redirect the user to a logout confirmation page or any other appropriate page
+        resp.sendRedirect("/unprotected");
+    }
+}
