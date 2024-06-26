9cc6f8ae98933813d380e0f2b4a9c16cae74766e
Jonathan <3190208@mail.zih.tu-dresden.de>
2024-06-26 18:19:55 +0200

    Add password salting

--- src/main/java/org/dbpedia/moss/db/UserDatabaseManager.java
+++ src/main/java/org/dbpedia/moss/db/UserDatabaseManager.java
@@ -6,8 +6,11 @@ import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 import java.sql.PreparedStatement;
 
+import org.mindrot.jbcrypt.BCrypt;
 
 public class UserDatabaseManager {
 
@@ -23,6 +26,18 @@ public class UserDatabaseManager {
 
     private String GetDatabaseURL() {
         return prefix + userDatabasePath;
+    }
+
+    private static String hashPassword(String plainTextPassword) {
+        return BCrypt.hashpw(plainTextPassword, BCrypt.gensalt());
+    }
+
+    private void checkPass(String plainTextPassword, String hashedPassword) {
+        if (BCrypt.checkpw(plainTextPassword, hashedPassword)) {
+            System.out.println("Matching password.");
+            return;
+        }
+        System.out.println("Password does not match");
     }
 
     public static void close(Connection connection) {
@@ -58,6 +73,30 @@ public class UserDatabaseManager {
         }
     }
 
+    public static HashMap<String, String> queryResult(Connection connection, String sql, String column) {
+        ResultSet resultSet;
+        HashMap<String, String> rows = new HashMap<>();
+        try {
+            resultSet = preparedStmt(connection, sql, column);
+
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            int columnCount = metaData.getColumnCount();
+
+            // Iterate through the result set
+            while (resultSet.next()) {
+                for (int i = 1; i <= columnCount; i++) {
+                    String columnName = metaData.getColumnName(i);
+                    String columnValue = resultSet.getString(i);
+                    rows.put(columnName, columnValue);
+                }
+            }
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+
+        return rows;
+    }
+
     public static boolean stmt(Connection connection, String sql) {
         boolean successful = false;
         try {
@@ -80,6 +119,35 @@ public class UserDatabaseManager {
         } catch (SQLException e) {
             System.err.println(e.getMessage());
         }
+    }
+
+    public static void preparedStmt(Connection connection, String sql, String key, String sub, String name) {
+        try {
+            PreparedStatement preparedStmt = connection.prepareStatement(sql);
+            preparedStmt.setString(1, key);
+            preparedStmt.setString(2, sub);
+            preparedStmt.setString(3, name);
+            int updatedRows = preparedStmt.executeUpdate();
+            System.out.println(String.format("Prepared statement:\n%s", preparedStmt.toString()));
+            System.out.println(String.format("Inserted rows %s", updatedRows));
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    public static ResultSet preparedStmt(Connection connection, String sql, String column) {
+        ResultSet resultSet;
+        try {
+            PreparedStatement preparedStmt = connection.prepareStatement(sql);
+            preparedStmt.setString(1, column);
+            // int updatedRows = preparedStmt.executeUpdate();
+            resultSet = preparedStmt.executeQuery();
+            System.out.println(String.format("Prepared statement:\n%s", preparedStmt.toString()));
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+            return null;
+        }
+        return resultSet;
     }
 
     private void createTable(String sql) {
@@ -121,6 +189,27 @@ public class UserDatabaseManager {
         }
     }
 
+    public HashMap<String, String> fetchQuery(String sql, String name) {
+        Connection conn = null;
+        HashMap<String, String> result = new HashMap<>();
+
+        try {
+            conn = DriverManager.getConnection(GetDatabaseURL());
+            System.out.println("Connection to SQLite has been established.");
+            System.out.println(String.format("Executing Statement:\n%s", sql));
+
+            result = queryResult(conn, sql, name);
+
+            System.out.println("Results");
+
+        } catch (SQLException sqlException) {
+            System.out.println(sqlException.getMessage());
+        } finally {
+            UserDatabaseManager.close(conn);
+        }
+        return result;
+    }
+
     public void insertQuery(String sql, String sub, String username) {
         Connection conn = null;
 
@@ -130,6 +219,23 @@ public class UserDatabaseManager {
             System.out.println(String.format("Executing Statement:\n%s", sql));
 
             preparedStmt(conn, sql, sub, username);
+
+        } catch (SQLException sqlException) {
+            System.out.println(sqlException.getMessage());
+        } finally {
+            UserDatabaseManager.close(conn);
+        }
+    }
+
+    public void insertQuery(String sql, String key, String sub, String name) {
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection(GetDatabaseURL());
+            System.out.println("Connection to SQLite has been established.");
+            System.out.println(String.format("Executing Statement:\n%s", sql));
+
+            preparedStmt(conn, sql, key, sub, name);
 
         } catch (SQLException sqlException) {
             System.out.println(sqlException.getMessage());
@@ -159,7 +265,8 @@ public class UserDatabaseManager {
         String sql = """
         CREATE TABLE IF NOT EXISTS api(
             key text PRIMARY KEY,
-            sub text NOT NULL
+            sub text NOT NULL,
+            name text NOT NULL
         );""";
         createTable(sql);
     }
@@ -167,5 +274,25 @@ public class UserDatabaseManager {
     public void insertUser(String sub, String username) {
         String sql = "INSERT INTO user(sub,name) VALUES(?, ?)";
         insertQuery(sql, sub, username);
+    }
+
+    public void insertKey(String key, String sub, String name) {
+        String sql = "INSERT INTO api(key, sub, name) VALUES(?, ?, ?)";
+        String saltedHashedKey = UserDatabaseManager.hashPassword(key);
+        String saltedHashSub = UserDatabaseManager.hashPassword(sub);
+        insertQuery(sql, saltedHashedKey, saltedHashSub, name);
+    }
+
+    public void getKey(String name) {
+        String sql = """
+        SELECT key, sub, name FROM api
+        WHERE name == ?
+        """;
+        HashMap<String, String> result = fetchQuery(sql, name);
+
+        for(Map.Entry<String, String> entry: result.entrySet()) {
+            System.out.println("key:" + entry.getKey());
+            System.out.println("value:" + entry.getValue());
+        }
     }
 }
